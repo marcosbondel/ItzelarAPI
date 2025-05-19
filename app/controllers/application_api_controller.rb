d8789de346253c4879ebd0e97100b53dbfd7264a
Marcos Bonifasi <marcosbonifasi19@gmail.com>
2025-05-19 01:50:27 -0600

    feat: revoke session

--- app/controllers/application_api_controller.rb
+++ app/controllers/application_api_controller.rb
@@ -17,9 +17,23 @@ class ApplicationApiController < ApplicationController
 
             return respond_with_unauthorized unless decoded
             
-            @current_session = User::Session.find_by(:user_uuid => decoded[0]['sub'], :session_uuid => decoded[0]['jti'])
+            @current_session = User::Session.find_by(
+                :user_uuid => decoded[0]['sub'], 
+                :session_uuid => decoded[0]['jti']
+            )
+
+            return respond_with_unauthorized "Please, sign in again" unless @current_session
             
-            return respond_with_unauthorized unless @current_session
+            # We check if the token has been revoked
+            if @current_session.revoked_at?
+                return respond_with_unauthorized 
+            end
+
+            # Invalid User-Agent (attack)
+            unless @current_session.user_agent.eql? request.user_agent
+                @current_session.update_attribute(:revoked_at, Time.current)
+                return respond_with_unauthorized 
+            end
 
             # Check if the token has expired
             if @current_session.expires_at < Time.now
@@ -27,6 +41,7 @@ class ApplicationApiController < ApplicationController
             end
 
             @current_user = @current_session.user
+            @current_session.update_attribute(:last_used_at, Time.current)
         end
 
         def user_not_authorized
